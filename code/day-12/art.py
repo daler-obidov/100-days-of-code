32586c4e560ab601274713749f43609e64bf5c49
Daler <dalerboyobidov@gmail.com>
2024-08-06 12:19:18 +0500

    day-12 TheGuessTheNumber Project

---
+++ code/day-12/art.py
@@ -0,0 +1,42 @@
+logo = '''                                                                                                                                                                                
+        GGGGGGGGGGGGG                                                                                                                                                                
+     GGG::::::::::::G                                                                                                                                                                
+   GG:::::::::::::::G                                                                                                                                                                
+  G:::::GGGGGGGG::::G                                                                                                                                                                
+ G:::::G       GGGGGGuuuuuu    uuuuuu      eeeeeeeeeeee        ssssssssss       ssssssssss          aaaaaaaaaaaaa        nnnn  nnnnnnnn    uuuuuu    uuuuuu     mmmmmmm    mmmmmmm   
+G:::::G              u::::u    u::::u    ee::::::::::::ee    ss::::::::::s    ss::::::::::s         a::::::::::::a       n:::nn::::::::nn  u::::u    u::::u   mm:::::::m  m:::::::mm 
+G:::::G              u::::u    u::::u   e::::::eeeee:::::eess:::::::::::::s ss:::::::::::::s        aaaaaaaaa:::::a      n::::::::::::::nn u::::u    u::::u  m::::::::::mm::::::::::m
+G:::::G    GGGGGGGGGGu::::u    u::::u  e::::::e     e:::::es::::::ssss:::::ss::::::ssss:::::s                a::::a      nn:::::::::::::::nu::::u    u::::u  m::::::::::::::::::::::m
+G:::::G    G::::::::Gu::::u    u::::u  e:::::::eeeee::::::e s:::::s  ssssss  s:::::s  ssssss          aaaaaaa:::::a        n:::::nnnn:::::nu::::u    u::::u  m:::::mmm::::::mmm:::::m
+G:::::G    GGGGG::::Gu::::u    u::::u  e:::::::::::::::::e    s::::::s         s::::::s             aa::::::::::::a        n::::n    n::::nu::::u    u::::u  m::::m   m::::m   m::::m
+G:::::G        G::::Gu::::u    u::::u  e::::::eeeeeeeeeee        s::::::s         s::::::s         a::::aaaa::::::a        n::::n    n::::nu::::u    u::::u  m::::m   m::::m   m::::m
+ G:::::G       G::::Gu:::::uuuu:::::u  e:::::::e           ssssss   s:::::s ssssss   s:::::s      a::::a    a:::::a        n::::n    n::::nu:::::uuuu:::::u  m::::m   m::::m   m::::m
+  G:::::GGGGGGGG::::Gu:::::::::::::::uue::::::::e          s:::::ssss::::::ss:::::ssss::::::s     a::::a    a:::::a        n::::n    n::::nu:::::::::::::::uum::::m   m::::m   m::::m
+   GG:::::::::::::::G u:::::::::::::::u e::::::::eeeeeeee  s::::::::::::::s s::::::::::::::s      a:::::aaaa::::::a        n::::n    n::::n u:::::::::::::::um::::m   m::::m   m::::m
+     GGG::::::GGG:::G  uu::::::::uu:::u  ee:::::::::::::e   s:::::::::::ss   s:::::::::::ss        a::::::::::aa:::a       n::::n    n::::n  uu::::::::uu:::um::::m   m::::m   m::::m
+        GGGGGG   GGGG    uuuuuuuu  uuuu    eeeeeeeeeeeeee    sssssssssss      sssssssssss           aaaaaaaaaa  aaaa       nnnnnn    nnnnnn    uuuuuuuu  uuuummmmmm   mmmmmm   mmmmmm
+'''
+
+congrat_logo = '''
+                                                                                                          iiii                   
+                                                                                                         i::::i                  
+                                                                                                          iiii                   
+                                                                                                                                 
+yyyyyyy           yyyyyyy ooooooooooo   uuuuuu    uuuuuu       wwwwwww           wwwww           wwwwwwwiiiiiiinnnn  nnnnnnnn    
+ y:::::y         y:::::yoo:::::::::::oo u::::u    u::::u        w:::::w         w:::::w         w:::::w i:::::in:::nn::::::::nn  
+  y:::::y       y:::::yo:::::::::::::::ou::::u    u::::u         w:::::w       w:::::::w       w:::::w   i::::in::::::::::::::nn 
+   y:::::y     y:::::y o:::::ooooo:::::ou::::u    u::::u          w:::::w     w:::::::::w     w:::::w    i::::inn:::::::::::::::n
+    y:::::y   y:::::y  o::::o     o::::ou::::u    u::::u           w:::::w   w:::::w:::::w   w:::::w     i::::i  n:::::nnnn:::::n
+     y:::::y y:::::y   o::::o     o::::ou::::u    u::::u            w:::::w w:::::w w:::::w w:::::w      i::::i  n::::n    n::::n
+      y:::::y:::::y    o::::o     o::::ou::::u    u::::u             w:::::w:::::w   w:::::w:::::w       i::::i  n::::n    n::::n
+       y:::::::::y     o::::o     o::::ou:::::uuuu:::::u              w:::::::::w     w:::::::::w        i::::i  n::::n    n::::n
+        y:::::::y      o:::::ooooo:::::ou:::::::::::::::uu             w:::::::w       w:::::::w        i::::::i n::::n    n::::n
+         y:::::y       o:::::::::::::::o u:::::::::::::::u              w:::::w         w:::::w         i::::::i n::::n    n::::n
+        y:::::y         oo:::::::::::oo   uu::::::::uu:::u               w:::w           w:::w          i::::::i n::::n    n::::n
+       y:::::y            ooooooooooo       uuuuuuuu  uuuu                www             www           iiiiiiii nnnnnn    nnnnnn
+      y:::::y                                                                                                                    
+     y:::::y                                                                                                                     
+    y:::::y                                                                                                                      
+   y:::::y                                                                                                                       
+  yyyyyyy 
+'''
