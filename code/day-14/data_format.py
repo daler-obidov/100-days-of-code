3bc67b7308d32d1e76558026c96920d10e5c4a4e
Daler <dalerboyobidov@gmail.com>
2024-08-08 13:36:47 +0500

    day-14

---
+++ code/day-14/data_format.py
@@ -0,0 +1,32 @@
+data = [
+    {
+    'name': 'Instagram',
+    'follower_count': '346',
+    'description': 'Social media platform',
+    'country': 'Unites States'    
+    },
+    {
+    'name': 'Cristiano Ronaldo',
+    'follower_count': '215',
+    'description': 'Footballer',
+    'country': 'Portugal'     
+    },
+    {
+    'name': 'Ariana Grande',
+    'follower_count': '183',
+    'description': 'Musician and actress',
+    'country': 'Unites States'  
+    },
+    {
+    'name': 'Dwayne Johns',
+    'follower_count': '181',
+    'description': 'Actor and professional wrestler',
+    'country': 'Unites States'     
+    },
+    {
+    'name': 'Daler',
+    'follower_count': '355',
+    'description': 'Man',
+    'country': 'Uzbekistan' 
+    }
+]
