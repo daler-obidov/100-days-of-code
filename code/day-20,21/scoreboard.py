d07a61732a9a5e261ba1a471708f9fad67d5250b
Daler <dalerboyobidov@gmail.com>
2024-08-15 13:53:19 +0500

    update day-21

--- code/day-20,21/scoreboard.py
+++ code/day-20,21/scoreboard.py
@@ -4,15 +4,22 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.penup()
         self.goto(0, 260)
         self.color('White')
         self.write(f"Score: {self.score}", align='center',  font=('Courier', 24, 'normal'))
         self.hideturtle()
         
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("Game Over.", align='center',  font=('Courier', 24, 'normal'))       
+    def update(self):
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align='center',  font=('Courier', 24, 'normal'))
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update()
+    
             
     def increase_score(self):
         self.score += 1
