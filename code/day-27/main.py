89d6f762e3f65f4607b4b8774e9547da99308873
daler-obidov <dalerboyobidov@gmail.com>
2024-08-20 16:42:46 +0500

    day-27

--- code/day-27/main.py
+++ code/day-27/main.py
@@ -1,1 +1,27 @@
-print("no Hi man g")
+from tkinter import *
+
+window = Tk()
+window.title("GUI")
+window.minsize(width=500, height=500)
+window.config(padx=100, pady=100)
+label = Label(text='HI', font=('Arial', 15, 'bold'))
+label.grid(column=0,row=0)
+
+def button_clicked():
+    print('Yeah')
+    new_text = input.get()
+    label.config(text=new_text)
+
+button = Button(text='hi man', command=button_clicked)
+button.grid(column=1,row=1)
+
+new_button = Button(text='No man')
+new_button.grid(column=2, row=0)
+
+input = Entry(width=10)
+input.grid(column=3,row=2)
+
+
+
+
+window.mainloop()
---
+++ code/day-27/mile_to_km.py
@@ -0,0 +1,32 @@
+from tkinter import *
+
+def miles_to_km():
+    miles = miles_input.get()
+    km = int(miles) * 1.609
+    rounded = round(km)
+    km_result.config(text=f'{rounded}')
+
+window = Tk()
+window.minsize(width=200, height=20)
+window.title('Miles to Kilometers')
+
+miles_input = Entry(width=10)
+miles_input.grid(column=1, row=0)
+
+miles_label = Label(text='Miles')
+miles_label.grid(column=4, row=0)
+
+is_equal = Label(text='is equal to')
+is_equal.grid(column=0, row=1)
+
+km_result = Label(text='0')
+km_result.grid(column=1, row=1)
+
+km_label = Label(text='Km')
+km_label.grid(column=4, row=1)
+
+calc_button = Button(text='calculate', command=miles_to_km)
+calc_button.grid(column=1, row=2)
+
+
+window.mainloop()
