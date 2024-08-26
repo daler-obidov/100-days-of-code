b6cba45c173846320a93c3ecfdedf270167c2be0
daler-obidov <dalerboyobidov@gmail.com>
2024-08-26 14:02:02 +0500

    update day-26

--- code/day-26/main.py
+++ code/day-26/main.py
@@ -5,6 +5,15 @@ data = pandas.read_csv('/home/daler/code/100-days-of-code/code/day-26/nato_phone
 
 phonetic_dic = {row.letter: row.code for (index, row) in data.iterrows()}
 
-word = input("Enter a word: ").upper()
-list = [phonetic_dic[letter] for letter in word]
-print(list)
+def generate():
+    word = input("Enter a word: ").upper()
+    try:
+        list = [phonetic_dic[letter] for letter in word]
+    except KeyError:
+        print('WTF man, go sleep')
+        generate()
+    else:
+
+        print(list)
+
+generate()
