f95e0af429031fac13499da8b2e2422f24ea9bbc
daler-obidov <dalerboyobidov@gmail.com>
2024-08-30 20:32:27 +0500

    update day34-35

--- code/day-34-35/ui.py
+++ code/day-34-35/ui.py
@@ -27,11 +27,11 @@ class QuizInterface:
         )
         self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
-        true_image = PhotoImage(file="code/day-34/images/true.png")
+        true_image = PhotoImage(file="code/day-34-35/images/true.png")
         self.true_button = Button(image=true_image, highlightthickness=0, command=self.true_pressed)
         self.true_button.grid(row=2, column=0)
 
-        false_image = PhotoImage(file="code/day-34/images/false.png")
+        false_image = PhotoImage(file="code/day-34-35/images/false.png")
         self.false_button = Button(image=false_image, highlightthickness=0, command=self.false_pressed)
         self.false_button.grid(row=2, column=1)
 
